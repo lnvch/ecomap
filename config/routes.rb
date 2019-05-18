4f274ba4b915c92fcc01eb2a822e3540daa34bbe
AliakseiLeanovich <aliaksei.leanovich@gmail.com>
2019-05-18 19:20:26 +0300

    Add Belarus regions to map

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'map#index'
 
   get 'map/index'
+  resources :regions, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
