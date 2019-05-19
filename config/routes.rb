7e2f554e45ee10261cd47aa47f53703041034a36
AliakseiLeanovich <aliaksei.leanovich@gmail.com>
2019-05-19 15:16:57 +0300

    Add layers

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'map/index'
   resources :regions, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trees, only: [:index]
+  resources :parks, only: [:index]
+  resources :lakes, only: [:index]
 end
